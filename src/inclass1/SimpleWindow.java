dddac5e0bb9f5e3a8b5870b55c1e9be721aaff20
jagstone <john.stone571@sie-1222-02.csuci.edu>
2015-08-31 13:31:16 -0700

    i was committed for too much crack

---
+++ src/inclass1/SimpleWindow.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+public class SimpleWindow extends JFrame
+{
+public static final int WIDTH = 300;
+public static final int HEIGHT = 200;
+public SimpleWindow()
+{
+setSize(WIDTH,HEIGHT);
+Container contentPane = getContentPane();
+JLabel label = new JLabel
+("You can display text using a label.");
+contentPane.add(label);
+contentPane.setBackground(Color.GREEN);
+addWindowListener(new WindowDestroyer());
+}
+public static void main(String[] args)
+{
+SimpleWindow window = new SimpleWindow();
+window.setVisible(true);
+}
+}
